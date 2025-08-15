21ab0acfe425afb9e861805c7132d7a7714ab6a4
maluv <maluv3106@gmail.com>
2025-08-15 08:26:24 -0300

    2 malu

---
+++ src/componets/Header.jsx
@@ -0,0 +1,17 @@
+export function Header(){
+    return(
+        <div className="border border-blue-500 flex flex-row justify-between">
+            <div className="">
+                <h1>AlzhCare</h1>
+            </div>
+
+            <div>
+                <a href="">O produto</a>
+                <a href="">Guia</a>
+                <a href="">Quem somos</a>
+                <a href="">Jogo</a>
+                <button>AAAA</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/componets/Home.jsx
@@ -0,0 +1,10 @@
+import vovo from "../assets/vovo.png"
+
+export function Home(){
+    return(
+        <div className="bg-[url('./assets/vovo.png')]">
+            
+            {/* <img src={vovo} alt="" /> */}
+        </div>
+    )
+} 
