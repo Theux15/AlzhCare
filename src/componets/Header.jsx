4d539a0903a32c278501218601cb3fe5221759bc
marialuizaVf <142500644+marialuizaVf@users.noreply.github.com>
2025-08-22 08:25:03 -0300

    8 malu

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,20 +1,28 @@
-import { useState } from "react"
 import { Link } from "react-router-dom"
+import { MoonIcon, SunDimIcon } from "@phosphor-icons/react"
+import { useEffect, useState } from "react"
 
 export function Header({ transparent = false }) {
-  const [isDark, setDark] = useState(false)
+    const [theme, setTheme] =useState(() => localStorage.getItem("theme") ?? "")
 
-  const AAAAA = () => {
-    setDark(!isDark)
-  }
+    useEffect(() => {
+      localStorage.setItem("theme", theme)
+      const tagHTML = document.documentElement
+      tagHTML.classList.remove("dark")
 
+
+      if(theme === "dark"){
+        tagHTML.classList.add("dark")
+      }
+    }, [theme])
   return (
-    <div className={isDark ? "dark" : ""}>
-
-      <div className={`
-    ${transparent ? "bg-transparent !bg-none" : "bg-light-purple dark:bg-dark-mode-purple"} 
-    h-24 px-6 flex items-center justify-between
-  `}>
+    <div>
+      <div
+        className={`
+          ${transparent ? "bg-transparent !bg-none" : "bg-light-purple dark:bg-dark-mode-purple"} 
+          h-24 px-6 flex items-center justify-between
+        `}
+      >
         <div className="flex flex-col w-[100vw]">
           <div id="texto" className="flex flex-row justify-between">
             <div className="text-dark-purple font-bold text-5xl dark:text-light-mode-purple">
@@ -26,45 +34,27 @@ export function Header({ transparent = false }) {
               <Link to="/guia">Guia</Link>
               <Link to="/quemSomos">Quem somos</Link>
               <Link to="/jogo">Jogo</Link>
-              
-
-
-              <button onClick={AAAAA}>
-                {isDark ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 text-black dark:text-white"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                  >
-                    <path d="M21 12.79A9 9 0 1 1 11.21 3A7 7 0 0 0 21 12.79z" />
-                  </svg>
-                ) : (<svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-yellow-500"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M12 2.625l-2.912 6.786-7.391.802 5.56 4.908-1.574 7.229 6.317-3.79 6.317 3.79-1.574-7.229 5.56-4.908-7.391-.802L12 2.625z"
-                  />
-                </svg>
-                )}
-              </button>
-
             </div>
           </div>
+          <button className="cursor-pointer" onClick={() => setTheme(prevTheme => prevTheme ==="dark" ? "light" : "dark")}>
+            {theme === "dark" ?(
+              <SunDimIcon size={32} weight="fill" className="text-yellow absolute-left-2" />
+            ) : (
+              <MoonIcon size={32} weight="fill" className="text-yellow absolute-right-2"/>
+            )}
+            <div
+              className={`absolute top-1 w-6 h-6 rounded-full bg-purple-700 transform transition-transform duration-300 ${
+               theme === "dark" ? "translate-x-8" : "translate-x-0"
+              }`}
+            ></div>
+          </button>
+
           <div>
             <div className="mt-4 mx-auto w-[97%] h-[2px] bg-dark-purple dark:bg-light-purple"></div>
           </div>
         </div>
       </div>
-
     </div>
-
-
-
-
   )
-
 }
+
