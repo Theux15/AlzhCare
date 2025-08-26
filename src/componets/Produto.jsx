b118e04872892fef40fbfca1ba9d81a7fc5dc534
Matheus Soares <macsoares1275@gmail.com>
2025-08-25 23:12:20 -0300

    [math] Estrutura e responsividade da página de Produtos finalizada.

--- src/componets/Produto.jsx
+++ src/componets/Produto.jsx
@@ -2,46 +2,94 @@ import { Header } from "./Header.jsx"
 
 export function Produto(){
     return(
+        
+
         <div className="bg-[var(--color-chatcolor)] dark:bg-[var(--color-dark-purple-home)]">
             <Header />
-            <div className="items-center justify-items-center flex flex-col gap-4 mt-5 mb-10 [color:var(--color-dark-purple)]">
-                <h1 className="text-7xl font-bold purple  ">Produto</h1>
+            <div className="items-center justify-items-center flex flex-col gap-4 mt-5 mb-5 [color:var(--color-dark-purple)]">
+                <h1 className="text-[38px] font-bold purple lg:text-7xl dark:[color:var(--color-light-purple)] ">Produto</h1>
 
-                <p className="text-2xl text-center max-w-3xl font-medium ">
+                <p className="text-[18px] text-center max-w-3xl font-medium lg:text-3xl dark:[color:var(--color-light-purple)]">
                     AlzhCare é um projeto criado para ajudar no cuidado de pessoas com Alzheimer, oferecendo apoio prático e informações úteis para familiares e cuidadores, e esse é nosso produto.
                 </p>
             </div>
 
             <hr />
 
-            <div className="flex flex-row items-center justify-center gap-20 mb-10">
-                <div className="w-full flex justify-end">
-                    <img className="rounded-lg w-[550px] h-[400px]" src="https://i.pinimg.com/736x/a6/ea/30/a6ea304f8b3b4fe1f89e58508bbe07b1.jpg" alt="" />
+            <div className="flex flex-row items-center gap-10 mb-5 mt-5 justify-center lg:grid lg:grid-cols-2 lg:pl-[10%] lg:pr-[10%]">
+                <div className="">
+                    <img className="rounded-xl w-[165px] h-[125px] lg:w-[550px] lg:h-[400px]" src="https://i.pinimg.com/736x/a6/ea/30/a6ea304f8b3b4fe1f89e58508bbe07b1.jpg" alt="" />
                 </div>
-                <div className="w-[80%]  [color:var(--color-dark-purple)] ">
-                    <p className="text-3xl font-semibold">AlzhCare</p>
-                    <p className="text-2xl font-semibold">Valor: R$ 00,00</p>
+                <div className=" [color:var(--color-dark-purple)] ">
+                    <p className="text-lg font-semibold lg:text-3xl text-center dark:[color:var(--color-white)]">AlzhCare</p>
+                    <p className="text-lg font-semibold lg:text-3xl text-center dark:[color:var(--color-white)] ">Valor: <br /> R$ 00,00</p>
                 </div>
-                
             </div>
 
             <hr />
 
-            <div>
-                <div className=" [color:var(--color-white)] bg-[var(--color-dark-purple)] flex flex-row rounded-2xl">
-                    <div className="">
-                        <img className="w-[150px] h-[100px] rounded-2xl" src="https://i.pinimg.com/736x/f5/a5/a8/f5a5a8f283a8a2120da1e9e686e7b0ac.jpg" alt="" />
+            <section className="flex flex-col lg:grid lg:grid-cols-2 lg:pl-[10%] lg:pr-[10%]">
+                <div className=" [color:var(--color-white)] flex flex-row rounded-2xl gap-10 mt-5 mb-5">
+                    <div className=" bg-[var(--color-dark-purple)] w-[165px] h-[125px] rounded-xl flex justify-center items-center lg:w-[260px] lg:h-[170px] dark:bg-[color:var(--color-light-purple)]">
+                        <img className="w-[150px] h-[100px] rounded-xl" src="https://i.pinimg.com/736x/f5/a5/a8/f5a5a8f283a8a2120da1e9e686e7b0ac.jpg" alt="" />
                     </div>
-                    <div>
-                        <h3>Esp32</h3>
-                        <p>Valor</p>
+                    <div className=" flex flex-col justify-center items-center  [color:var(--color-dark-purple)]">
+                        <h3 className="text-lg font-medium lg:text-3xl dark:[color:var(--color-white)] ">Esp32</h3>
+                        <p className="text-lg font-medium text-center lg:text-3xl dark:[color:var(--color-white)]">Valor: <br /> R$ 00,00</p>
                     </div>
                 </div>
-                
-                <div>
 
+                <div className=" [color:var(--color-white)] flex flex-row rounded-2xl gap-10 mt-5 mb-5">
+                    <div className=" bg-[var(--color-dark-purple)] w-[165px] h-[125px] rounded-xl flex justify-center items-center lg:w-[260px] lg:h-[170px] dark:bg-[color:var(--color-light-purple)]">
+                        <img className="w-[150px] h-[100px] rounded-xl" src="https://i.pinimg.com/736x/f5/a5/a8/f5a5a8f283a8a2120da1e9e686e7b0ac.jpg" alt="" />
+                    </div>
+                    <div className=" flex flex-col justify-center items-center  [color:var(--color-dark-purple)]">
+                        <h3 className="text-lg font-medium lg:text-3xl dark:[color:var(--color-white)] ">Esp32</h3>
+                        <p className="text-lg font-medium text-center lg:text-3xl dark:[color:var(--color-white)]">Valor: <br /> R$ 00,00</p>
+                    </div>
                 </div>
-            </div>
+
+                <div className=" [color:var(--color-white)] flex flex-row rounded-2xl gap-10 mt-5 mb-5">
+                    <div className=" bg-[var(--color-dark-purple)] w-[165px] h-[125px] rounded-xl flex justify-center items-center lg:w-[260px] lg:h-[170px] dark:bg-[color:var(--color-light-purple)]">
+                        <img className="w-[150px] h-[100px] rounded-xl" src="https://i.pinimg.com/736x/f5/a5/a8/f5a5a8f283a8a2120da1e9e686e7b0ac.jpg" alt="" />
+                    </div>
+                    <div className=" flex flex-col justify-center items-center  [color:var(--color-dark-purple)]">
+                        <h3 className="text-lg font-medium lg:text-3xl dark:[color:var(--color-white)] ">Esp32</h3>
+                        <p className="text-lg font-medium text-center lg:text-3xl dark:[color:var(--color-white)]">Valor: <br /> R$ 00,00</p>
+                    </div>
+                </div>
+
+                <div className=" [color:var(--color-white)] flex flex-row rounded-2xl gap-10 mt-5 mb-5">
+                    <div className=" bg-[var(--color-dark-purple)] w-[165px] h-[125px] rounded-xl flex justify-center items-center lg:w-[260px] lg:h-[170px] dark:bg-[color:var(--color-light-purple)]">
+                        <img className="w-[150px] h-[100px] rounded-xl" src="https://i.pinimg.com/736x/f5/a5/a8/f5a5a8f283a8a2120da1e9e686e7b0ac.jpg" alt="" />
+                    </div>
+                    <div className=" flex flex-col justify-center items-center  [color:var(--color-dark-purple)]">
+                        <h3 className="text-lg font-medium lg:text-3xl dark:[color:var(--color-white)] ">Esp32</h3>
+                        <p className="text-lg font-medium text-center lg:text-3xl dark:[color:var(--color-white)]">Valor: <br /> R$ 00,00</p>
+                    </div>
+                </div>
+
+                <div className=" [color:var(--color-white)] flex flex-row rounded-2xl gap-10 mt-5 mb-5">
+                    <div className=" bg-[var(--color-dark-purple)] w-[165px] h-[125px] rounded-xl flex justify-center items-center lg:w-[260px] lg:h-[170px] dark:bg-[color:var(--color-light-purple)]">
+                        <img className="w-[150px] h-[100px] rounded-xl" src="https://i.pinimg.com/736x/f5/a5/a8/f5a5a8f283a8a2120da1e9e686e7b0ac.jpg" alt="" />
+                    </div>
+                    <div className=" flex flex-col justify-center items-center  [color:var(--color-dark-purple)]">
+                        <h3 className="text-lg font-medium lg:text-3xl dark:[color:var(--color-white)] ">Esp32</h3>
+                        <p className="text-lg font-medium text-center lg:text-3xl dark:[color:var(--color-white)]">Valor: <br /> R$ 00,00</p>
+                    </div>
+                </div>
+
+                <div className=" [color:var(--color-white)] flex flex-row rounded-2xl gap-10 mt-5 mb-5">
+                    <div className=" bg-[var(--color-dark-purple)] w-[165px] h-[125px] rounded-xl flex justify-center items-center lg:w-[260px] lg:h-[170px] dark:bg-[color:var(--color-light-purple)]">
+                        <img className="w-[150px] h-[100px] rounded-xl" src="https://i.pinimg.com/736x/f5/a5/a8/f5a5a8f283a8a2120da1e9e686e7b0ac.jpg" alt="" />
+                    </div>
+                    <div className=" flex flex-col justify-center items-center  [color:var(--color-dark-purple)]">
+                        <h3 className="text-lg font-medium lg:text-3xl dark:[color:var(--color-white)] ">Esp32</h3>
+                        <p className="text-lg font-medium text-center lg:text-3xl dark:[color:var(--color-white)]">Valor: <br /> R$ 00,00</p>
+                    </div>
+                </div> 
+
+            </section>
 
         </div>
     )
