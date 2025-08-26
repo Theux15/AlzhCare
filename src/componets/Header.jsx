1e3c23ff0f83ab817d4f0f419e6fa2eb48f827d0
PEDRO HENRIQUE DUARTE 22A TI <135224527+P3dro2704@users.noreply.github.com>
2025-08-25 22:06:57 -0300

    25/08 pt 2

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -21,39 +21,37 @@ export function Header({ transparent = false }) {
         transparent ? "bg-transparent !bg-none" : "bg-light-purple dark:bg-dark-mode-purple"
       } h-20 px-6 flex flex-col`}
     >
-
       <div className="flex items-center justify-between w-full h-full">
-        
-
+        {/* Logo isolada */}
         <div className="text-dark-purple font-bold text-3xl dark:text-light-mode-purple">
           <Link to="/">AlzhCare</Link>
         </div>
 
+        {/* Links + botão juntos */}
+        <div className="flex items-center gap-5">
+          <nav className="flex gap-4 text-dark-purple font-semibold text-lg dark:text-light-mode-purple">
+            <Link to="/produto">O produto</Link>
+            <Link to="/guiainstrucao">Guia</Link>
+            <Link to="/quemSomos">Quem somos</Link>
+            <Link to="/jogo">Jogo</Link>
+          </nav>
 
-        <nav className="flex gap-6 text-dark-purple font-semibold text-lg dark:text-light-mode-purple">
-          <Link to="/produto">O produto</Link>
-          <Link to="/guiainstrucao">Guia</Link>
-          <Link to="/quemSomos">Quem somos</Link>
-          <Link to="/jogo">Jogo</Link>
-        </nav>
-
-
-        <button
-          className={`relative w-14 h-8 bg-purple-700 rounded-full p-1 flex items-center transition-all focus:outline-none ${
-            theme === "dark" ? "justify-end" : "justify-start"
-          }`}
-          onClick={() => setTheme((prev) => (prev === "dark" ? "light" : "dark"))}
-        >
-          {theme === "dark" ? (
-            <SunDimIcon size={24} className="text-yellow" />
-          ) : (
-            <MoonIcon size={24} className="text-yellow" />
-          )}
-        </button>
+          <button
+            className={`relative w-14 h-8 bg-purple-700 rounded-full p-1 flex items-center transition-all focus:outline-none cursor-pointer ${
+              theme === "dark" ? "justify-end" : "justify-start"
+            }`}
+            onClick={() => setTheme((prev) => (prev === "dark" ? "light" : "dark"))}
+          >
+            {theme === "dark" ? (
+              <SunDimIcon size={24} className="text-yellow" />
+            ) : (
+              <MoonIcon size={24} className="text-yellow" />
+            )}
+          </button>
+        </div>
       </div>
 
-
-      <div className=" w-full h-[1px] bg-dark-purple dark:bg-light-purple -mt-2 "></div>
+      <div className="w-full h-[1px] bg-dark-purple dark:bg-light-purple -mt-2"></div>
     </header>
   );
 }
