e02569015cb6c3104cb4428eef90f54daa42c83d
Matheus Soares <macsoares1275@gmail.com>
2025-09-10 07:31:14 -0300

    [math] Página de produto finalizada e corrigida!

--- src/componets/ComponentesProduto.jsx
+++ src/componets/ComponentesProduto.jsx
@@ -1,12 +1,12 @@
 export function ComponentesProduto({ imgSrc, title, value }) {
   return (
     
-            <div className=" [color:var(--color-white)] flex flex-row rounded-2xl gap-10 mt-5 mb-5">
+            <div className=" [color:var(--color-white)] flex flex-row rounded-2xl gap-8 mt-5 mb-5">
                     <div className=" bg-[var(--color-dark-purple)] w-[165px] h-[125px] rounded-xl flex justify-center items-center lg:w-[260px] lg:h-[170px] dark:bg-[color:var(--color-light-purple)]">
-                        <img className="max-w-[200px]  max-h-[100px] rounded-xl" src={imgSrc} alt="" />
+                        <img className="max-w-[150px] lg:max-w-[200px]  max-h-[100px] rounded-xl" src={imgSrc} alt="" />
                     </div>
-                    <div className=" flex flex-col justify-center items-center  [color:var(--color-dark-purple)]">
-                        <h3 className="text-lg font-medium lg:text-3xl dark:[color:var(--color-white)] ">{title}</h3>
+                    <div className=" flex flex-col justify-center items-center w-full max-w-[40%] [color:var(--color-dark-purple)]">
+                        <h3 className="text-lg font-medium lg:text-3xl dark:[color:var(--color-white)] text-center break-words hyphens-auto max-w-full">{title}</h3>
                         <p className="text-lg font-medium text-center lg:text-3xl dark:[color:var(--color-white)]">Valor: <br /> {value}</p>
                     </div>
                 </div>
--- src/componets/Produto.jsx
+++ src/componets/Produto.jsx
@@ -11,6 +11,7 @@ import ModuloGSM from '../assets/ModuloGSM.png';
 import Protoboard from '../assets/Protoboard.png';
 import RealTimeClock from '../assets/RealTimeClock.png';
 import SensorCardiaco from '../assets/SensorCardiaco.png';
+import { ResponsiveHeader } from "./ResponsiveHeader.jsx";
 
 
 export function Produto(){
@@ -18,22 +19,23 @@ export function Produto(){
         
 
         <div className="bg-[var(--color-chatcolor)] dark:bg-[var(--color-dark-purple-home)]">   
-            <Header />
+            <ResponsiveHeader/>
             <div className="items-center justify-items-center flex flex-col gap-4 mt-20 mb-15 [color:var(--color-dark-purple)]">
                 <h1 className="font-poppins text-[38px] font-bold purple lg:text-7xl dark:[color:var(--color-light-purple)] ">Produto</h1>
 
-                <p className="font-inter text-[18px] text-center max-w-3xl font-medium lg:text-3xl dark:[color:var(--color-light-purple)]">
+                <p className="font-inter text-[18px] px-5 text-center max-w-3xl font-medium lg:text-3xl dark:[color:var(--color-light-purple)]">
                     AlzhCare é um projeto criado para ajudar no cuidado de pessoas com Alzheimer, oferecendo apoio prático e informações úteis para familiares e cuidadores, e esse é nosso produto.
                 </p>
             </div>
 
 
 
-            <div className="flex flex-row items-center gap-10 mb-5 mt-5 justify-center lg:grid lg:grid-cols-2 lg:pl-[10%] lg:pr-[10%]">
+
+            <div className="flex flex-row items-center gap-10 mb-5 mt-5 justify-center lg:grid lg:grid-cols-2 lg: mx-[5%]">
                 <div className="">
                     <img className="rounded-xl w-[165px] h-[125px] lg:w-[550px] lg:h-[400px]" src="https://i.pinimg.com/736x/a6/ea/30/a6ea304f8b3b4fe1f89e58508bbe07b1.jpg" alt="" />
                 </div>
-                <div className=" [color:var(--color-dark-purple)] ">
+                <div className=" [color:var(--color-dark-purple)] w-full max-w-[40%] flex flex-col justify-center items-center">
                     <p className="text-lg font-semibold lg:text-3xl text-center dark:[color:var(--color-white)]">AlzhCare</p>
                     <p className="text-lg font-semibold lg:text-3xl text-center dark:[color:var(--color-white)] ">Valor: <br /> R$ 00,00</p>
                 </div>
@@ -51,7 +53,7 @@ export function Produto(){
 
                 <ComponentesProduto
                     imgSrc={AcelerometroGiroscopio}
-                    title="Acelerômetro/Giroscópio"
+                    title="Acelerômetro"
                     value="R$ 00,00"
                 />
 
@@ -104,7 +106,7 @@ export function Produto(){
                 />
 
             </section>
-            <Link to="/sidemenu" className="block p-4 hover:bg-[var(--color-dark-purple)] dark:hover:bg-[var(--color-light-purple)]">Menu Lateral</Link>
+            
         </div>
     )
 }  
