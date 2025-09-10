b4a1ceec86b6f63623d975ddb12d1f8dbb273b20
Giovanna Ferreira 11ATI <gigita.gita6baby@gmail.com>
2025-09-09 23:02:55 -0300

    Gi-100% pronto

--- src/componets/Jogo.jsx
+++ src/componets/Jogo.jsx
@@ -8,62 +8,61 @@ export function Jogo() {
     return (
         <div className="bg-[var(--color-chatcolor)] dark:bg-[var(--color-dark-purple-home)]">
             <div
-                className="h-[150vh] w-full bg-cover bg-center flex flex-col"
+                className="min-h-screen w-full bg-cover bg-center flex flex-col"
                 style={{ backgroundImage: `url(${Galaxia})` }}
             >
 
                 <Header />
 
 
-                <div
-                    className="flex flex-col items-center justify-center flex-1 text-center px-4 sm:px-6"
-
-                >
+                <div className="flex flex-col items-center justify-center flex-1 text-center px-4">
                     <h1
-                        className="text-white font-bold 
-                                   lg:text-[5.5rem] sm:text-5xl md:text-7xl 
-                                   leading-tight max-w-2xl"
-
+                        className="text-white font-bold text-4xl lg:text-[5.5rem]  leading-tight max-w-2xl"
                     >
                         MindJourney
                     </h1>
 
                     <p
-                        className="font-inter text-sm sm:text-base md:text-xl lg:text-[1.5rem] 
-                                   text-white mt-6 max-w-3xl"
-
+                        className="font-inter text-base lg:text-[1.5rem] text-white mt-6 max-w-md lg:max-w-3xl font-regular"
                     >
                         MindJourney é um jogo interativo em que seu personagem deve enfrentar os desafios e puzzles do cérebro humano para tentar recuperar memórias perdidas no subconsciente.
                     </p>
 
                     <button
-                        className="w-[225px] h-[65px] mt-10 bg-[var(--color-purple-home)] text-white text-[1.25rem] font-bold rounded-full shadow-md hover:opacity-90 transition"
-
+                        className="w-40 h-12 mt-8 lg:w-[18rem] lg:h-[5rem] bg-[var(--color-purple-home)] text-white text-base lg:text-[1.5rem] font-bold rounded-full shadow-md hover:opacity-90 transition"
                     >
-                        Jogar
+                        JOGAR
                     </button>
-
-
-
-
                 </div>
-
-            </div>
-            <div className="lg:w-[67.5rem] lg:h-[40rem] mx-auto mt-[-20rem] w-40 h-40 bg-[var(--color-light-purple)] rounded-2xl flex items-center justify-center hover:scale-105 transition">
-                <img src={Play} alt="Play" className="w-16 h-16" />
             </div>
 
-            <div className="bg-[var(--color-chatcolor)] dark:bg-[var(--color-dark-purple-home)]">
-                <div className=" items-center justify-items-center flex flex-col gap-4 mt-20 mb-15 [color:var(--color-dark-purple)]">
+            <div className="w-72 h-44 
+                lg:w-[67.5rem] lg:h-[40rem] 
+                mx-auto -mt-16 lg:-mt-32 
+                bg-[var(--color-light-purple)] rounded-2xl flex items-center justify-center 
+                hover:scale-105 transition">
+                <img src={Play} alt="Play"
+                    className="w-12 h-12 lg:w-[9.3rem] lg:h-[9.3rem]" />
+            </div>
 
-                    <h1 className="font-poppins text-[2.5rem] font-bold purple lg:text-[4.5rem] dark:[color:var(--color-white)] ">A História</h1>
 
-                    <p className="font-inter text-[1.5rem] text-center max-w-3xl font-medium lg:text-[1.5rem] dark:[color:var(--color-white)]">
+            <div className="bg-[var(--color-chatcolor)] dark:bg-[var(--color-dark-purple-home)] px-4">
+                <div className="items-center justify-items-center flex flex-col gap-6 mt-20 mb-16 text-center [color:var(--color-dark-purple)]">
+                    <h1 className="font-poppins text-2xl lg:text-[4.5rem] font-bold text-purple dark:text-white">
+                        A História
+                    </h1>
+
+                    <p className="font-inter text-base lg:text-xl text-center max-w-md lg:max-w-3xl font-regular dark:text-white mx-auto">
                         MindJourney se passa dentro da mente fragmentada de uma pessoa com Alzheimer. Quatro personagens exploram esse mundo perdido, resolvendo puzzles para recuperar fragmentos de memória espalhados. Cada lembrança resgatada ajuda a montar o quebra-cabeça da identidade perdida, enquanto enfrentam desafios que refletem as dificuldades da doença. Com visual pixel art 2D, o jogo convida o jogador a uma jornada emocional pela mente humana em busca da memória e da esperança.
                     </p>
 
-                    <div className="lg:w-[31.5rem] lg:h-[20.5rem] mx-auto mt-[2rem] w-40 h-40 bg-[color:var(--color-light-purple)] rounded-2xl flex items-center justify-center hover:scale-105 transition">
-                        <img src={Fotomais} alt="Fotomais" className="w-16 h-16" />
+                    <div className="w-40 h-32 
+                        lg:w-[31.5rem] lg:h-[20.5rem] 
+                        mx-auto mt-4 lg:mt-8 
+                        bg-[var(--color-light-purple)] rounded-2xl 
+                        flex items-center justify-center hover:scale-105 transition">
+                        <img src={Fotomais} alt="Fotomais"
+                            className="w-12 h-12 lg:w-[6.25rem] lg:h-[6.25rem]" />
                     </div>
                 </div>
             </div>
--- src/componets/Referencias.jsx
+++ src/componets/Referencias.jsx
@@ -3,50 +3,63 @@ import { Footer } from "./Footer"
 
 export function Referencias() {
    return (
-            <div className="bg-[var(--color-chatcolor)] dark:bg-[var(--color-dark-purple-home)]">   
+        <div className="bg-[var(--color-chatcolor)] dark:bg-[var(--color-dark-purple-home)]">   
             <Header />
-            <div className=" items-center justify-items-center flex flex-col gap-4 mt-20 mb-15 [color:var(--color-dark-purple)]">
-                <h1 className="font-poppins text-[38px] font-bold purple lg:text-7xl dark:[color:var(--color-light-purple)] ">Referencias</h1>
 
-                <p className="font-inter text-[20px] text-center max-w-3xl font-bold lg:text-3xl dark:[color:var(--color-light-purple)]">
-                   Estudo sobre os estágios iniciais do Alzheimer e métodos de intervenção cognitiva. 
-                </p>
-                
-                <p className="font-inter text-[20px] text-center max-w-3xl font-medium lg:text-3xl dark:[color:var(--color-light-purple)]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed dictum quam.https://doi.org/10.1000/alzheimer-intervention-2024
-                </p>
-                <p className="font-inter text-[20px] text-center max-w-3xl font-bold lg:text-3xl dark:[color:var(--color-light-purple)]">
-                    Análise do uso de tecnologia assistiva em pacientes com demência.
-                </p>
-                <p className="font-inter text-[20px] text-center max-w-3xl font-medium lg:text-3xl dark:[color:var(--color-light-purple)]">
-                    Donec vel sapien nec erat tincidunt euismod. Vestibulum ante ipsum primis in faucibus.https://doi.org/10.1000/assistive-tech-study
-                </p>
-                <p className="font-inter text-[20px] text-center max-w-3xl font-bold lg:text-3xl dark:[color:var(--color-light-purple)]">
-                    Guia clínico para cuidadores de pessoas com Alzheimer.
-                </p>
-                <p className="font-inter text-[20px] text-center max-w-3xl font-medium lg:text-3xl dark:[color:var(--color-light-purple)]">
-                    Pellentesque ac orci vehicula, dapibus sem at, scelerisque orci.https://doi.org/10.1000/caregiver-guide-2023
-                </p>
-                <p className="font-inter text-[20px] text-center max-w-3xl font-bold lg:text-3xl dark:[color:var(--color-light-purple)]">
-                    Artigo sobre estimulação auditiva e visual no tratamento de Alzheimer.
-                </p>
-                <p className="font-inter text-[20px] text-center max-w-3xl font-medium lg:text-3xl dark:[color:var(--color-light-purple)]">
-                    Morbi fermentum augue nec luctus posuere. Integer sollicitudin libero ut sapien hendrerit.https://doi.org/10.1000/audio-visual-stimulation
-                </p>
+            <div className="flex flex-col items-center mt-20 mb-16 [color:var(--color-dark-purple)] px-4">
+                <h1 className="font-poppins text-[2.5rem] lg:text-[85px] font-bold purple dark:[color:var(--color-dark-text)] mb-10 text-center">
+                    Referências
+                </h1>
 
-                
+                <div className="mb-8 text-center max-w-xs lg:max-w-5xl">
+                    <p className="font-inter text-[18px] lg:text-[24px] font-bold dark:[color:var(--color-white)] mb-1">
+                        Estudo sobre os estágios iniciais do Alzheimer e métodos de intervenção cognitiva.
+                    </p>
+                    <p className="font-inter text-[18px] lg:text-[24px] font-regular dark:[color:var(--color-white)]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed dictum quam. 
+                        <br />
+                        https://doi.org/10.1000/alzheimer-intervention-2024
+                    </p>
+                </div>
+
+
+                <div className="mb-8 text-center max-w-xs lg:max-w-5xl">
+                    <p className="font-inter text-[18px] lg:text-[24px] font-bold dark:[color:var(--color-white)] mb-1">
+                        Análise do uso de tecnologia assistiva em pacientes com demência.
+                    </p>
+                    <p className="font-inter text-[18px] lg:text-[24px] font-regular dark:[color:var(--color-white)]">
+                        Donec vel sapien nec erat tincidunt euismod. Vestibulum ante ipsum primis in faucibus.
+                        <br />
+                        https://doi.org/10.1000/assistive-tech-study
+                    </p>
+                </div>
+
+
+                <div className="mb-8 text-center max-w-xs lg:max-w-5xl">
+                    <p className="font-inter text-[18px] lg:text-[24px] font-bold dark:[color:var(--color-white)] mb-1">
+                        Guia clínico para cuidadores de pessoas com Alzheimer.
+                    </p>
+                    <p className="font-inter text-[18px] lg:text-[24px] font-regular dark:[color:var(--color-white)]">
+                        Pellentesque ac orci vehicula, dapibus sem at, scelerisque orci.
+                        <br />
+                        https://doi.org/10.1000/caregiver-guide-2023
+                    </p>
+                </div>
+
+
+                <div className="mb-8 text-center max-w-xs lg:max-w-5xl">
+                    <p className="font-inter text-[18px] lg:text-[24px] font-bold dark:[color:var(--color-white)] mb-1">
+                        Artigo sobre estimulação auditiva e visual no tratamento de Alzheimer.
+                    </p>
+                    <p className="font-inter text-[18px] lg:text-[24px] font-regular dark:[color:var(--color-white)]">
+                        Morbi fermentum augue nec luctus posuere. Integer sollicitudin libero ut sapien hendrerit.
+                        <br />
+                        https://doi.org/10.1000/audio-visual-stimulation
+                    </p>
+                </div>
             </div>
-            <Footer/>
+
+            <Footer />
         </div>
    )
 }
-
-
-
-                    
-                    
-
-                    
-                    
-                    
-                    
