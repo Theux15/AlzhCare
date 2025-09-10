1b61f77479d1b935e9edba13df478681524c3d47
Matheus Soares <macsoares1275@gmail.com>
2025-09-10 07:17:51 -0300

    [math] Header mobile ajustado no modo transparente e adicionado linha

--- src/componets/HeaderMobile.jsx
+++ src/componets/HeaderMobile.jsx
@@ -6,23 +6,36 @@ export function HeaderMobile({ transparent = false, onMenuToggle }) {
     <header
       className={`${
         transparent ? "bg-transparent !bg-none" : "bg-light-purple dark:bg-dark-mode-purple"
-      } h-16 px-4 flex items-center justify-between`}
+      } h-16 px-4 flex flex-col`}
     >
-      
-      <button
-        onClick={onMenuToggle}
-        className="text-dark-purple dark:text-light-mode-purple p-2"
-      >
-        <ListIcon size={28} />
-      </button>
+      <div className="flex items-center justify-between w-full h-full">
+        <button
+          onClick={onMenuToggle}
+          className={`p-2 ${
+            transparent 
+              ? "text-white dark:text-white" 
+              : "text-dark-purple dark:text-light-mode-purple"
+          }`}
+        >
+          <ListIcon size={28} />
+        </button>
 
-      
-      <div className="text-dark-purple font-poppins font-semibold text-2xl dark:text-light-mode-purple">
-        <Link to="/">AlzhCare</Link>
+        <div className={`font-poppins font-semibold text-2xl ${
+          transparent 
+            ? "text-white dark:text-white" 
+            : "text-dark-purple dark:text-light-mode-purple"
+        }`}>
+          <Link to="/">AlzhCare</Link>
+        </div>
+
+        <div className="w-12"></div>
       </div>
 
-      
-      <div className="w-12"></div>
+      <div className={`w-full h-[1px] -mt-2 ${
+        transparent 
+          ? "bg-white dark:bg-white" 
+          : "bg-dark-purple dark:bg-light-purple"
+      }`}></div>
     </header>
   );
 }
